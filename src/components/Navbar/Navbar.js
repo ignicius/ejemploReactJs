efc4159f246b2c1191c358a35a1e6d72928ad06b
ignicius <pratoignacio@gmail.com>
2022-11-19 17:29:45 -0300

    agregando carpeta components

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,11 @@
+const Navbar = () => {
+    return (
+        <nav>
+            <h1>MyApp</h1>
+            <button>boton 1</button>
+            <button>boton 2</button>
+        </nav>
+    )
+}
+
+export default Navbar
